4da33d31a83a38ec475e824769fc00cc5b249b7a
rcsoyer <rcsoyer@gmail>
2025-03-07 18:08:36 +0100

    simple refactoring

--- src/main/java/org/acme/telemetry_service/TelemetryServiceApplication.java
+++ src/main/java/org/acme/telemetry_service/TelemetryServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TelemetryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TelemetryServiceApplication.class, args);
-	}
-
+    public static void main(final String[] args) {
+        SpringApplication.run(TelemetryServiceApplication.class, args);
+    }
 }
