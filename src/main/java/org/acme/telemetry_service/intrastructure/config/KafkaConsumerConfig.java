b0194f4780fb90692dc1edc77581f193bdfde0fd
rcsoyer <rcsoyer@gmail>
2025-03-07 19:52:58 +0100

    simple refactoring

--- src/main/java/org/acme/telemetry_service/intrastructure/config/KafkaConsumerConfig.java
+++ src/main/java/org/acme/telemetry_service/intrastructure/config/KafkaConsumerConfig.java
@@ -1,7 +1,6 @@
 package org.acme.telemetry_service.intrastructure.config;
 
 import java.util.Map;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +9,11 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
+import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
+
 @EnableKafka
 @Configuration
 class KafkaConsumerConfig {
@@ -17,22 +21,16 @@ class KafkaConsumerConfig {
     @Bean
     ConsumerFactory<String, String> consumerFactory() {
         final Map<String, Object> configs =
-          Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                 "bootstrapAddress",
-                 ConsumerConfig.GROUP_ID_CONFIG,
-                 "groupId",
-                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                 StringDeserializer.class,
-                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                 StringDeserializer.class);
+          Map.of(BOOTSTRAP_SERVERS_CONFIG, "bootstrapAddress",
+                 GROUP_ID_CONFIG, "groupId",
+                 KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                 VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(configs);
     }
 
     @Bean
-    ConcurrentKafkaListenerContainerFactory<String, String>
-    kafkaListenerContainerFactory() {
-        final var factory =
-          new ConcurrentKafkaListenerContainerFactory<String, String>();
+    ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        final var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
