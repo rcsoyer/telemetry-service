f64ad8dfe8ba95575c5902c052796000934fbf4e
rcsoyer <rcsoyer@gmail>
2025-03-08 01:00:44 +0100

    set up the configuration that allow for automatic detection of the type of the kafka handler

---
+++ src/main/java/org/acme/telemetry_service/domain/dto/command/FridgeTelemetryEvent.java
@@ -0,0 +1,4 @@
+package org.acme.telemetry_service.domain.dto.command;
+
+public record FridgeTelemetryEvent() {
+}
--- src/main/java/org/acme/telemetry_service/intrastructure/config/KafkaConsumerConfig.java
+++ src/main/java/org/acme/telemetry_service/intrastructure/config/KafkaConsumerConfig.java
@@ -8,34 +8,44 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
-import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
+import static org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper.TypePrecedence.INFERRED;
 
 @EnableKafka
 @Configuration
 class KafkaConsumerConfig {
 
     @Bean
+    RecordMessageConverter multiTypeConverter() {
+        final var typeMapper = new DefaultJackson2JavaTypeMapper();
+        typeMapper.setTypePrecedence(INFERRED);
+        typeMapper.addTrustedPackages("org.acme.telemetry_service.domain.dto.command");
+        final var messageConverter = new StringJsonMessageConverter();
+        messageConverter.setTypeMapper(typeMapper);
+        return messageConverter;
+    }
+
+    @Bean
     ConsumerFactory<String, Object> consumerFactory() {
         final Map<String, Object> configs =
           Map.of(BOOTSTRAP_SERVERS_CONFIG, "bootstrapAddress",
-                 GROUP_ID_CONFIG, "groupId",
                  KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                 VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>
-                 (configs,
-                  new StringDeserializer(),
-                  new JsonDeserializer<>());
+                 VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configs);
     }
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
         final var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
         factory.setConsumerFactory(consumerFactory());
+        factory.setRecordMessageConverter(multiTypeConverter());
         return factory;
     }
 }
