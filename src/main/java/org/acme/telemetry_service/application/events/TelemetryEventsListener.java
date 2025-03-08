e82acf339073b49dde8332eb53d8fd882df2dc2f
rcsoyer <rcsoyer@gmail>
2025-03-08 14:43:35 +0100

    wip - just a log for now to test it

--- src/main/java/org/acme/telemetry_service/application/events/TelemetryEventsListener.java
+++ src/main/java/org/acme/telemetry_service/application/events/TelemetryEventsListener.java
@@ -1,15 +1,18 @@
 package org.acme.telemetry_service.application.events;
 
+import lombok.extern.slf4j.Slf4j;
 import org.acme.telemetry_service.domain.dto.command.FridgeTelemetryEvent;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @KafkaListener(id = "telemetryGroup", topics = "telemetryEvents")
 class TelemetryEventsListener {
 
     @KafkaHandler
     void fridgeTelemetryEventHandler(final FridgeTelemetryEvent event) {
+        log.debug("fridge telemetry event={}", event);
     }
 }
