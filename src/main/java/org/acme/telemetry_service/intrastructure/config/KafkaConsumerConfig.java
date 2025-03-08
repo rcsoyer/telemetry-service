a7a66277ab9675b478886aecb0938495f79626c3
rcsoyer <rcsoyer@gmail>
2025-03-08 01:32:08 +0100

    set the kafka taskExecutor to use virtual threads

--- src/main/java/org/acme/telemetry_service/intrastructure/config/KafkaConsumerConfig.java
+++ src/main/java/org/acme/telemetry_service/intrastructure/config/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -44,10 +45,16 @@ class KafkaConsumerConfig {
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-      final ConsumerFactory<String, Object> consumerFactory) {
+      final ConsumerFactory<String, Object> consumerFactory,
+      final AsyncTaskExecutor virtualThreadAsyncTaskExecutor,
+      final RecordMessageConverter multiTypeConverter) {
         final var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
         factory.setConsumerFactory(consumerFactory);
-        factory.setRecordMessageConverter(multiTypeConverter());
+        factory.setRecordMessageConverter(multiTypeConverter);
+        factory.setContainerCustomizer(customizer -> {
+            customizer.getContainerProperties()
+                      .setListenerTaskExecutor(virtualThreadAsyncTaskExecutor);
+        });
         return factory;
     }
 }
