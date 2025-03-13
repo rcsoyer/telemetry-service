fe37a6c85893d00d8e726231ab73855c04890655
rcsoyer <rcsoyer@gmail>
2025-03-13 20:53:16 +0100

    separation of concerns and clean code - make the event handler call the service layer to persist events

--- src/main/java/org/acme/telemetryservice/application/events/TelemetryEventsHandler.java
+++ src/main/java/org/acme/telemetryservice/application/events/TelemetryEventsHandler.java
@@ -1,19 +1,25 @@
 package org.acme.telemetryservice.application.events;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.acme.telemetryservice.domain.dto.command.CoffeeMachineTelemetryData;
 import org.acme.telemetryservice.domain.dto.command.FridgeTelemetryData;
 import org.acme.telemetryservice.domain.dto.command.ThermostatTelemetryData;
+import org.acme.telemetryservice.domain.service.FridgeTelemetryService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 class TelemetryEventsHandler {
+
+    private final FridgeTelemetryService fridgeTelemetryService;
 
     @KafkaListener(topics = "fridgeEvents")
     void fridgeTelemetryEventHandler(final FridgeTelemetryData event) {
         log.info("fridge telemetry event={}", event);
+        fridgeTelemetryService.createTelemetryEvent(event);
     }
 
     @KafkaListener(topics = "coffeeMachineEvents")
