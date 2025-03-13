02a1787f14333eab1ebb893da5e8e5eae4581b56
rcsoyer <rcsoyer@gmail>
2025-03-13 20:52:26 +0100

    add a simple method on the repository to fetch IoT entities by the natural ID

--- src/main/java/org/acme/telemetryservice/infrastructure/repository/IoTDeviceRepository.java
+++ src/main/java/org/acme/telemetryservice/infrastructure/repository/IoTDeviceRepository.java
@@ -1,9 +1,13 @@
 package org.acme.telemetryservice.infrastructure.repository;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.acme.telemetryservice.domain.entity.IoTDevice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IoTDeviceRepository extends JpaRepository<IoTDevice, Long> {
+
+    Optional<IoTDevice> findByDeviceId(UUID deviceId);
 }
