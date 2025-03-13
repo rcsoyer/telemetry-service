86197ad06ea31a709e6d914a1b177782d3fb50e4
rcsoyer <rcsoyer@gmail>
2025-03-13 23:28:09 +0100

    add new efficient method that simply makes a question to the database

--- src/main/java/org/acme/telemetryservice/infrastructure/repository/IoTDeviceRepository.java
+++ src/main/java/org/acme/telemetryservice/infrastructure/repository/IoTDeviceRepository.java
@@ -10,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface IoTDeviceRepository extends JpaRepository<IoTDevice, Long> {
 
     Optional<IoTDevice> findByDeviceId(UUID deviceId);
+
+    boolean existsByDeviceName(String deviceName);
 }
