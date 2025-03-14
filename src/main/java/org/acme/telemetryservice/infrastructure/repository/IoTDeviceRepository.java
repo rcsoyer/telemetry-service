93c3d2009cb6e138dcccb4078fe50a29a12d0e6f
rcsoyer <rcsoyer@gmail>
2025-03-14 15:31:33 +0100

    add missing query hint to hit the Hibernate L2 cache

--- src/main/java/org/acme/telemetryservice/infrastructure/repository/IoTDeviceRepository.java
+++ src/main/java/org/acme/telemetryservice/infrastructure/repository/IoTDeviceRepository.java
@@ -1,14 +1,17 @@
 package org.acme.telemetryservice.infrastructure.repository;
 
+import jakarta.persistence.QueryHint;
 import java.util.Optional;
 import java.util.UUID;
 import org.acme.telemetryservice.domain.entity.IoTDevice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IoTDeviceRepository extends JpaRepository<IoTDevice, Long> {
 
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
     Optional<IoTDevice> findByDeviceId(UUID deviceId);
 
     boolean existsByDeviceName(String deviceName);
