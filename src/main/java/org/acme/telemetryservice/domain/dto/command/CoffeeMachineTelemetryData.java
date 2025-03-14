78e418f1d1854a89efb1ae6e882ea8111ee0a337
rcsoyer <rcsoyer@gmail>
2025-03-14 23:58:12 +0100

    add spring validator to kafka configuration

--- src/main/java/org/acme/telemetryservice/domain/dto/command/CoffeeMachineTelemetryData.java
+++ src/main/java/org/acme/telemetryservice/domain/dto/command/CoffeeMachineTelemetryData.java
@@ -1,9 +1,11 @@
 package org.acme.telemetryservice.domain.dto.command;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import org.acme.telemetryservice.domain.entity.CoffeeMachineTelemetryEvent.DeviceStatus;
 
 public record CoffeeMachineTelemetryData(
+  @NotNull(message = "The source device's ID is mandatory")
   UUID deviceId,
   @EnumType(enumClass = DeviceStatus.class)
   String status) implements TelemetryData {
