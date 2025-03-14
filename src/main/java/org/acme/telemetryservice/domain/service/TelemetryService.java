ad83f1fef14951671e32e8e9a85f28686753ecf5
rcsoyer <rcsoyer@gmail>
2025-03-14 22:29:33 +0100

    implement a higher abstraction layer to reuse more code

--- src/main/java/org/acme/telemetryservice/domain/service/TelemetryService.java
+++ src/main/java/org/acme/telemetryservice/domain/service/TelemetryService.java
@@ -1,11 +1,14 @@
 package org.acme.telemetryservice.domain.service;
 
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.acme.telemetryservice.domain.dto.command.TelemetryData;
 import org.acme.telemetryservice.domain.entity.BaseAuditEventEntity;
 import org.acme.telemetryservice.domain.entity.IoTDevice;
 import org.acme.telemetryservice.infrastructure.repository.IoTDeviceRepository;
 
+@Slf4j
 @RequiredArgsConstructor
 abstract class TelemetryService<E extends BaseAuditEventEntity,
                                  T extends TelemetryData> {
@@ -17,7 +20,12 @@ abstract class TelemetryService<E extends BaseAuditEventEntity,
           .findByDeviceId(event.deviceId())
           .map(sourceDevice -> mapToEntity(sourceDevice, event))
           .map(this::persistTelemetryEvent)
-          .orElseThrow(() -> new IllegalArgumentException("Device not found"));
+          .orElseThrow(() -> iotSourceDeviceNotFound(event.deviceId()));
+    }
+
+    private IllegalArgumentException iotSourceDeviceNotFound(final UUID sourceId) {
+        log.warn("Invalid deviceId={} passed as event source", sourceId);
+        return new IllegalArgumentException("Device not found");
     }
 
     abstract E mapToEntity(IoTDevice sourceDevice, T telemetryData);
