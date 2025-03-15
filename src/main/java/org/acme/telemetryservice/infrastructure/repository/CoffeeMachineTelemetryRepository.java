3d80142d483ad57a1baae81a5d54d502d5225daa
rcsoyer <rcsoyer@gmail>
2025-03-16 00:23:16 +0100

    wip - adjusting the returned data

--- src/main/java/org/acme/telemetryservice/infrastructure/repository/CoffeeMachineTelemetryRepository.java
+++ src/main/java/org/acme/telemetryservice/infrastructure/repository/CoffeeMachineTelemetryRepository.java
@@ -1,11 +1,13 @@
 package org.acme.telemetryservice.infrastructure.repository;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 import org.acme.telemetryservice.domain.dto.query.CoffeeMachineStatusSummary;
 import org.acme.telemetryservice.domain.entity.CoffeeMachineTelemetryEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,8 +18,14 @@ public interface CoffeeMachineTelemetryRepository
              + "COUNT(telemetryEvent) AS totalCount, "
              + "telemetryEvent.status AS deviceStatus "
              + "FROM CoffeeMachineTelemetryEvent telemetryEvent "
-             + "WHERE telemetryEvent.sourceDevice.deviceId = ?1 "
+             + "WHERE telemetryEvent.sourceDevice.deviceId = :deviceId "
              + "AND telemetryEvent.status IN ('READY', 'ERROR') "
-             + "GROUP BY deviceId, deviceStatus")
-    List<CoffeeMachineStatusSummary> getMachineEventsSummary(UUID deviceId);
+             + "AND (cast(:startDate as timestamp) is null "
+             + "AND cast(:endDate as timestamp) is null) "
+             + "OR (telemetryEvent.createdAt BETWEEN :startDate AND :endDate) "
+             + "GROUP BY telemetryEvent.sourceDevice.deviceId, telemetryEvent.status")
+    List<CoffeeMachineStatusSummary> getMachineEventsSummary(
+      @Param("deviceId") UUID deviceId,
+      @Param("startDate") Instant startDate,
+      @Param("endDate") Instant endDate);
 }
