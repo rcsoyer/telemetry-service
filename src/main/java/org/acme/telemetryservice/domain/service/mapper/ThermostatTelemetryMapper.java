4ac800effebf83c3b03d98d048066f292d253971
rcsoyer <rcsoyer@gmail>
2025-03-15 12:25:47 +0100

    simple refactoring - remove the need for another annotation

--- src/main/java/org/acme/telemetryservice/domain/service/mapper/ThermostatTelemetryMapper.java
+++ src/main/java/org/acme/telemetryservice/domain/service/mapper/ThermostatTelemetryMapper.java
@@ -4,13 +4,10 @@ import org.acme.telemetryservice.domain.dto.command.ThermostatTelemetryData;
 import org.acme.telemetryservice.domain.entity.IoTDevice;
 import org.acme.telemetryservice.domain.entity.ThermostatTelemetryEvent;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper
 public interface ThermostatTelemetryMapper {
 
-    @Mapping(target = "sourceDevice", source = "ioTDevice")
-    ThermostatTelemetryEvent from(
-      final IoTDevice ioTDevice,
-      final ThermostatTelemetryData telemetryData);
+    ThermostatTelemetryEvent from(final IoTDevice sourceDevice,
+                                  final ThermostatTelemetryData telemetryData);
 }
