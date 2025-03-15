35f73329a738284d24a758fda5fbe7d6afb87abe
rcsoyer <rcsoyer@gmail>
2025-03-15 13:34:06 +0100

    refactoring - making Optional in case of not existing the data

--- src/main/java/org/acme/telemetryservice/domain/service/CoffeeMachineTelemetryService.java
+++ src/main/java/org/acme/telemetryservice/domain/service/CoffeeMachineTelemetryService.java
@@ -44,7 +44,7 @@ public non-sealed class CoffeeMachineTelemetryService extends TelemetryService<C
 
     private Supplier<ResponseStatusException> noDeviceFound(final UUID deviceId) {
         return () -> {
-            log.warn("Client of the API provided a deviceId that it's not registered."
+            log.warn("Client of the API provided a deviceId that it's not registered. "
                        + "Or the machine never finished making coffees. "
                        + "deviceId={}", deviceId);
             return new ResponseStatusException(NOT_FOUND, "No information available for this device");
