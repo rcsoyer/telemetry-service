d9ef88c91b9121cb3a3af05deaa6045900f9b6ff
rcsoyer <rcsoyer@gmail>
2025-03-16 10:23:34 +0100

    wip - implementing persistence layer tests

--- src/test/java/org/acme/telemetryservice/infrastructure/repository/CoffeeMachineTelemetryRepositoryTest.java
+++ src/test/java/org/acme/telemetryservice/infrastructure/repository/CoffeeMachineTelemetryRepositoryTest.java
@@ -1,19 +1,60 @@
 package org.acme.telemetryservice.infrastructure.repository;
 
 import java.time.Instant;
-import java.util.UUID;
+import java.util.List;
+import org.acme.telemetryservice.domain.dto.query.CoffeeMachineStatusSummary;
+import org.acme.telemetryservice.domain.entity.CoffeeMachineTelemetryEvent;
+import org.acme.telemetryservice.domain.entity.IoTDevice;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.acme.telemetryservice.domain.entity.CoffeeMachineTelemetryEvent.DeviceStatus.READY;
+import static org.acme.telemetryservice.domain.entity.IoTDevice.IotDeviceType.COFFEE_MACHINE;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CoffeeMachineTelemetryRepositoryTest extends BaseRepositoryTest {
 
     @Autowired
     private CoffeeMachineTelemetryRepository repository;
 
-    @Test
-    void getMachineEventsSummary() {
-        repository.getMachineEventsSummary(UUID.randomUUID(), Instant.now(), null);
+    @Autowired
+    private IoTDeviceRepository ioTDeviceRepository;
+
+    private IoTDevice sourceCoffeeMachine;
+
+    @BeforeEach
+    void setUp() {
+        this.sourceCoffeeMachine = IoTDevice
+                                     .builder()
+                                     .deviceType(COFFEE_MACHINE)
+                                     .deviceModel("Coffee Machine 200xyz Lavaza")
+                                     .deviceName("House coffee maker")
+                                     .build();
+        ioTDeviceRepository.save(sourceCoffeeMachine);
+    }
+
+    @Nested
+    class GetMachineEventsSummary {
+
+        @Test
+        void getMachineEventsSummary_allFilters() {
+            final var telemetryEvent = CoffeeMachineTelemetryEvent
+                                         .builder()
+                                         .sourceDevice(sourceCoffeeMachine)
+                                         .status(READY)
+                                         .build();
+            repository.save(telemetryEvent);
+
+            final List<CoffeeMachineStatusSummary> result =
+              repository.getMachineEventsSummary(sourceCoffeeMachine.getDeviceId(),
+                                                 Instant.parse("2025-01-01T10:15:30.00Z"),
+                                                 Instant.now());
+
+            assertThat(result)
+              .hasSize(1);
+        }
+
     }
 }
