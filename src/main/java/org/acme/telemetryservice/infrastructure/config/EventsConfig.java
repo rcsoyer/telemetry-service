5850642dbad19e7cf7a4e962d6511cb6a2093e7a
rcsoyer <rcsoyer@gmail>
2025-03-16 19:03:50 +0100

    simple refactoring - renaming methods

--- src/main/java/org/acme/telemetryservice/infrastructure/config/EventsConfig.java
+++ src/main/java/org/acme/telemetryservice/infrastructure/config/EventsConfig.java
@@ -25,7 +25,8 @@ class EventsConfig {
     }
 
     @Bean
-    AuthenticationEventPublisher authenticationEventPublisher(final ApplicationEventPublisher applicationEventPublisher) {
+    AuthenticationEventPublisher authenticationEventPublisher(
+      final ApplicationEventPublisher applicationEventPublisher) {
         return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
     }
 }
